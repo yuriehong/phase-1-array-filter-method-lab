114ccfa1499d962df93da09c8b5d306e68199951
+ <+>
2022-07-21 17:39:57 -0400

    commit tree 92e5e6791d56929166f338e22a9befacbefbaf22

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(function (driver){
+         return driver.toUpperCase() == name.toUpperCase();
+    })
+
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(function (driver) {
+        return driver.substring(0,name.length) == name;
+    })
+
+
+}
+
+function matchName(driverObjects, name){
+    return driverObjects.filter(function (driver){
+        return driver.name == name;
+    })
+
+}
